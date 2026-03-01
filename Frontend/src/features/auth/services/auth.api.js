5305a1296da18b5a6a085efd305c458552dda224
Tohit khan <153702980+tohit09Fst@users.noreply.github.com>
2026-03-01 18:55:19 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://jobfit-analyzer-19ca.onrender.com",
     withCredentials: true
 })
 
