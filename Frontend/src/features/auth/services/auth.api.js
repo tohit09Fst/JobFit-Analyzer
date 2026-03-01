326a9dbbe1ed1edd6608fd1bea3f6f0d4d2d03d8
Tohit khan <153702980+tohit09Fst@users.noreply.github.com>
2026-03-01 19:23:45 +0530

    Change API base URL to localhost for development

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://jobfit-analyzer-19ca.onrender.com",
+    baseURL: "http://localhost:3000",
     withCredentials: true
 })
 
