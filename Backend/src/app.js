8a526f5759e6758d6c4be4d20a0a17217a859b6f
Tohit khan <153702980+tohit09Fst@users.noreply.github.com>
2026-03-01 19:23:02 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://job-fit-analyzer-gamma.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
